d19f5ec1f2fd32e4746f166ff8406848d52d2b64
Yusraashraf44 <yusraash44@gmail.com>
2024-10-07 14:18:15 +0530

    Add files via upload

---
+++ Sep30-0ct4/positive.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number:"))
+if(num>=0):
+    if(num==0):
+       print("The number is zero")
+    else:
+        print("The number is positive")
+else:
+    print("The number is negative")

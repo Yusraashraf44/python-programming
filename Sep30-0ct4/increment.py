30ed54a07a97a6ea4853e79180c3dae6bc7ed9cb
Yusraashraf44 <yusraash44@gmail.com>
2024-10-07 14:00:32 +0530

    Add files via upload

---
+++ Sep30-0ct4/increment.py
@@ -0,0 +1,5 @@
+num=int(input("enter a number:"))
+num=num+5
+print("number after first increment",num)
+num=num-3
+print("number after second increment",num)
---
+++ Sep30-0ct4/percentage.py
@@ -0,0 +1,11 @@
+math=int(input("Enter marks for maths out of 50:"))
+phy=int(input("Enter marks for phy out of 50:"))
+eng=int(input("Enter marks for eng out of 50:"))
+chem=int(input("Enter marks for chem out of 50:"))
+bio=int(input("Enter marks for bio out of 50:"))
+total=math+phy+eng+chem+bio #total mark obtained
+print(total)
+total1=5*50 #total mark
+perc=total/total1*100
+print(perc)
+

b6111d5fd59feb09fad568c5ce25dd53e7469b9f
Yusraashraf44 <yusraash44@gmail.com>
2024-10-07 13:49:37 +0530

    Add files via upload

---
+++ sep 23-27/armoperations.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number:"))
+b=int(input("enetr the number:"))
+sum=a+b
+difference=a-b
+product=a*b
+quotient=a/b
+print("sum :",sum)
+print("difference:",difference)
+print("product:",product)
+print("quotient:",quotient)

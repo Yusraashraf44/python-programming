03b69cab65085815d9228a2d1d0b9c70cee50cc6
YUSRA <yusraash44@gmail.com>
2024-10-18 16:18:18 +0530

    Delete Oct-18/firstnames.py

--- Oct-18/firstnames.py
+++
@@ -1,7 +0,0 @@
-names=[]
-word=input("Enter first names separated by spaces: ")
-names.append(word)
-a_count=0
-for name in names:
-    a_count=name.lower().count('a')
-    print("The letter 'a' appears ",a_count," times in the list")

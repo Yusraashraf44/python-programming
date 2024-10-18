4058e64a1060613884a34b43cf131f8056cd581e
YUSRA <yusraash44@gmail.com>
2024-10-18 16:16:33 +0530

    Add files via upload

---
+++ Oct-18/empty.py
@@ -0,0 +1,7 @@
+list=[]
+a=int(input("enter the number of elements to be inserted:"))
+print("enter the elements:")
+for i in range(a):
+      element=int(input( ))
+      list.append(element)
+print(list)

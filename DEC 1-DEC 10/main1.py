572072870d7ecf257647cf71514bb6ff20d7a6a8
YUSRA <yusraash44@gmail.com>
2024-12-12 16:12:08 +0530

    Add files via upload

---
+++ DEC 1-DEC 10/main1.py
@@ -0,0 +1,12 @@
+# main1.py
+from graphics.rectangle import *
+from graphics.circle import *
+import graphics.threeDgraphics.cuboid as cuboid
+import graphics.threeDgraphics.sphere as sphere
+# For Rectangle
+print("RECTANGLE AREA:",area_r(2,5))
+print("Rectangle perimeter:",perimeter_r(2,6))
+print("Circle area:",area(5))
+print("Circle perimeter:",perimeter(4))
+print("CUBOID:")
+print("SPHERE:")
